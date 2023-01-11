912e4dd65b6690b0065ec050cd790a282c24edaa
brenley-woodard <brenleyew@gmail.com>
2023-01-11 14:23:50 -0800

    font style is rendering to preview

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,14 +8,15 @@ import './Home.css';
 export default function Home() {
   const [title, setTitle] = useState('');
   const [subtitle, setSubtitle] = useState('');
+  const [font, setFont] = useState('');
   
   // add useState calls here for title, subtitle, font, align, and text
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview {...{ title, subtitle }}/>
-      <Editor {...{ setTitle, setSubtitle }}/>
+      <Preview {...{ title, subtitle, font }}/>
+      <Editor {...{ setTitle, setSubtitle, setFont }}/>
     </main>
   );
 }
