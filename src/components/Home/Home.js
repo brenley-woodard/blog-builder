e8be7e66b2b6e8ea4208da3c41e314545c24e7c6
brenley-woodard <brenleyew@gmail.com>
2023-01-11 12:46:24 -0800

    subtitle input is stored in state and rendered to preview

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,13 +7,15 @@ import './Home.css';
 
 export default function Home() {
   const [title, setTitle] = useState('');
+  const [subtitle, setSubtitle] = useState('');
+  
   // add useState calls here for title, subtitle, font, align, and text
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview {...{ title, setTitle }}/>
-      <Editor {...{ title, setTitle }}/>
+      <Preview {...{ title, subtitle }}/>
+      <Editor {...{ setTitle, setSubtitle }}/>
     </main>
   );
 }
